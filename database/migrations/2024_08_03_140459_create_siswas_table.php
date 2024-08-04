b6c717cfe88e79130fd931a614c95343fd7c0994
rubyfeel <faris.rfeel12@gmail.com>
2024-08-04 09:19:06 +0700

    pull deploy

---
+++ database/migrations/2024_08_03_140459_create_siswas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->string('kelas_uuid');
+            $table->string('nama_siswa');
+            $table->string('jenis_kelamin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswas');
+    }
+};
