7d539d9a03e074dd2fc7353099de35484c27ae08
rubyfeel <faris.rfeel12@gmail.com>
2024-08-13 20:03:19 +0700

    Revisi Fitur Video

--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -10,7 +10,7 @@
                             <a href="/video/create" class="btn btn-primary">Tambah Video</a>
                         </div>
                         <div class="card-body">
-                            <table id="table-user" class="table table-striped dt-responsive nowrap w-100">
+                            <table id="table-user" class="table table-striped table-bordered table-responsive">
                                 <thead>
                                     <tr class="text-center">
                                     <tr>
