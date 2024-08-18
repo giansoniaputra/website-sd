1dd2f91b6de06f3385b61fad11fd7fa203caa905
rubyfeel <faris.rfeel12@gmail.com>
2024-08-18 15:10:32 +0700

    Revisi Edit Crop Berita

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -144,7 +144,7 @@
 
             modal.on('shown.bs.modal', function() {
                 cropper = new Cropper(image, {
-                    aspectRatio: 1 / 1,
+                    aspectRatio: 7 / 4,
                     preview: '.preview'
                 })
             }).on('hidden.bs.modal', function() {
